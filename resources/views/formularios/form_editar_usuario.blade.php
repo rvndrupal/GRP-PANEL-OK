b7694eb79791a50aa6793d0a2ba9d7756b8a31fb
rvndrupal <rodrigodrupal1@gmail.com>
2019-04-11 10:20:30 -0500

    Se configura el menu administración

--- resources/views/formularios/form_editar_usuario.blade.php
+++ resources/views/formularios/form_editar_usuario.blade.php
@@ -6,6 +6,7 @@
 <div class="row" >
 
 <div class="col-md-12">
+    <a href="#"  class="btn btn-sm btn-danger cerrarModal" style="margin: 0 0 6px 1224px;" onclick="cerrarModal();"> X </a>
 
   <div class="box box-primary box-gris">
     <div class="box-header with-border my-box-header">
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -45,6 +45,14 @@
                     <li><a href="#"></a></li>
                 </ul>
             </li>
+
+            <li class="treeview">
+                    <a href="#"><i class='fa fa-lock'></i> <span>ADMINISTRAR</span> <i class="fa fa-angle-left pull-right"></i></a>
+                    <ul class="treeview-menu">
+                        <li><a href="javascript:void(0);"  onclick="cargar_formulario(2);">Roles</a></li>
+                        <li><a href="javascript:void(0);"  onclick="cargar_formulario(3);" >Permisos</a></li>
+                    </ul>
+                </li>
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
